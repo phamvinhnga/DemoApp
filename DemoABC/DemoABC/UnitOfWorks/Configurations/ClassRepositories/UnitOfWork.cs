c81eaa28ec41c7d7d3cdcb0ae14b1a051163c0a8
Puanqq <quang.huuvinhnguyen@infodation.vn>
2022-03-05 21:12:38 +0700

    Unit of work & Log by Serilog

---
+++ DemoABC/DemoABC/UnitOfWorks/Configurations/ClassRepositories/UnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿using DemoABC.UnitOfWorks.Configurations.InterfaceRepositories;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace DemoABC.UnitOfWorks.Configurations.ClassRepositories
+{
+    public class UnitOfWork<TContext> : IUnitOfWork where TContext : DbContext
+    {
+        protected readonly TContext context;
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+        public virtual ValueTask DisposeAsync()
+        {
+            return context.DisposeAsync();
+        }
+        public int SaveChange()
+        {
+            try
+            {
+                return context.SaveChanges();
+            }
+            catch (Exception ex)
+            {                
+                return 0;
+            }
+        }
+
+        public Task<int> SaveChangeAsync()
+        {
+            try
+            {
+                return context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {                
+                return Task.FromResult(0);
+            }
+        }
+    }
+}
